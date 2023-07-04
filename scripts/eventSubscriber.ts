050c2c4ee5ee899c92e8c723ef8f40b41ae05547
Randy Pen <pzm16@tsinghua.org.cn>
2023-07-04 16:47:37 +0800

    wss

--- scripts/eventSubscriber.ts
+++ scripts/eventSubscriber.ts
@@ -1,6 +1,14 @@
-import { JsonRpcProvider, testnetConnection, SuiEvent } from 'npm:@mysten/sui.js';
+// import { JsonRpcProvider, testnetConnection, SuiEvent } from 'npm:@mysten/sui.js';
+
+import { JsonRpcProvider, Connection, SuiEvent } from 'npm:@mysten/sui.js';
+
+// Construct your connection:
+const connection = new Connection({
+	fullnode: 'wss://fullnode.testnet.sui.io',
+	faucet: 'wss://faucet.testnet.sui.io/gas',
+});
 // connect to Testnet
-const provider = new JsonRpcProvider(testnetConnection);
+const provider = new JsonRpcProvider(connection);
 
 const promptPackage = '0x86e2ab6c370fbfed0ee955158ca95ca5b465dede4a79eb3594d2959e72d3d62a';
 const promptFilter = { MoveModule: { package: promptPackage, module: 'cybrosnetwork' } };
